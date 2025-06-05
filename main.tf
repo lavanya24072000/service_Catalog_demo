305a6cdc97bcd6de206aef90fbb07506541931ca
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-06-05 16:07:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
